316d550c16ced32ad8d3a98d4ba6aae69e94532e
Marielene <annienoncheva@yahoo.com>
2015-09-07 13:14:30 +0300

    Vry little progress

---
+++ MiniProject00/Calculator/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Calculator
+{
+	class MainClass
+	{
+		//Defining Libraries
+		public Userinput.Uinput uInput=new Userinput.Uinput ();
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Hello World!");
+			MainClass _program = new MainClass ();
+			_program.uInput.justesting ();
+		}
+	}
+}
---
+++ MiniProject00/Userinput/Userinput.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Userinput
+{
+	public class Uinput	
+	{
+		public Uinput ()
+		{
+		}
+		public void justesting()
+		{
+			Console.WriteLine ("startin Userinput.Uinput.justesting()");
+		}
+	}
+}
+
